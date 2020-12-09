6dd52fbf8a767facb3fb650200ff454753655d5e
David Maia <davidkmaia@gmail.com>
2020-12-09 18:19:39 -0300

    first commit of store microservice

---
+++ store-ms/src/main/java/com/spring/cloud/microservices/sample/storems/StoreMsApplication.java
@@ -0,0 +1,13 @@
+package com.spring.cloud.microservices.sample.storems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreMsApplication.class, args);
+	}
+
+}
---
+++ store-ms/src/test/java/com/spring/cloud/microservices/sample/storems/StoreMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.cloud.microservices.sample.storems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
