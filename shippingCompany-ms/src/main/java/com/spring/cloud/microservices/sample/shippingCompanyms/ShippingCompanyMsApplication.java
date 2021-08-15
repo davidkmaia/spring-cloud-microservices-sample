c6d9b211f7051903206030c4e65347f39910c139
David Maia <davidkmaia@gmail.com>
2021-08-15 14:48:56 -0300

    Created a empty project of shipping company

---
+++ shippingCompany-ms/src/main/java/com/spring/cloud/microservices/sample/shippingCompanyms/ShippingCompanyMsApplication.java
@@ -0,0 +1,13 @@
+package com.spring.cloud.microservices.sample.shippingCompanyms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShippingCompanyMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShippingCompanyMsApplication.class, args);
+	}
+
+}
---
+++ shippingCompany-ms/src/test/java/com/spring/cloud/microservices/sample/shippingCompanyms/ShippingCompanyMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.cloud.microservices.sample.shippingCompanyms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShippingCompanyMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
