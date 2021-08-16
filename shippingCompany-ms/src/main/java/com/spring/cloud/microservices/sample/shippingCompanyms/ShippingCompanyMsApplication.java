daebb9d9af163e5f8740e4c3486f3867ce6c57a3
David Maia <davidkmaia@gmail.com>
2021-08-16 19:08:31 -0300

    Configured to use feign

--- shippingCompany-ms/src/main/java/com/spring/cloud/microservices/sample/shippingCompanyms/ShippingCompanyMsApplication.java
+++ shippingCompany-ms/src/main/java/com/spring/cloud/microservices/sample/shippingCompanyms/ShippingCompanyMsApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ShippingCompanyMsApplication {
 
 	public static void main(String[] args) {
+		System.setProperty("spring.cloud.bootstrap.enabled","true");
 		SpringApplication.run(ShippingCompanyMsApplication.class, args);
 	}
 
